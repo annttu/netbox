f54b619a97cf7ba94e4d87e0598160d88f39d527
Antti Jaakkola <antti.jaakkola@remod.fi>
2017-02-22 17:41:12 +0200

    links to table

--- netbox/virtual/tables.py
+++ netbox/virtual/tables.py
@@ -8,13 +8,26 @@ from utilities.tables import BaseTable, ToggleColumn
 from virtual.models import VirtualMachine, VirtualMachineGroup, VirtualCluster
 
 
+STATUS_ICON = """
+{% if record.status %}
+    <span class="glyphicon glyphicon-ok-sign text-success" title="Active" aria-hidden="true"></span>
+{% else %}
+    <span class="glyphicon glyphicon-minus-sign text-danger" title="Offline" aria-hidden="true"></span>
+{% endif %}
+"""
+
+
 class VirtualMachineTable(BaseTable):
     pk = ToggleColumn()
     name = tables.LinkColumn('virtual:virtual_machine', args=[Accessor('pk')], verbose_name='Name')
+    status = tables.TemplateColumn(template_code=STATUS_ICON, verbose_name='')
+    virtual_cluster = tables.LinkColumn('virtual:virtual_cluster', args=[Accessor('virtual_cluster.pk')], verbose_name='Virtual Cluster')
+    tenant = tables.LinkColumn('tenancy:tenant', args=[Accessor('tenant.slug')], verbose_name='Tenant')
+    group = tables.LinkColumn('virtual:virtual_machine_group', args=[Accessor('group.pk')], verbose_name='Group')
 
     class Meta(BaseTable.Meta):
         model = VirtualMachine
-        fields = ('pk', 'name', 'description', 'group', 'tenant')
+        fields = ('pk', 'name', 'status', 'description', 'group', 'tenant', 'virtual_cluster')
 
 
 class VirtualMachineGroupTable(BaseTable):
